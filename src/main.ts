cdc48da7e07b3b88d8793aa549dd4a6884375e8e
AutomoJapan <bike.racer.sabid@gmail.com>
2026-05-08 14:36:40 +0600

    initial project setup - nestJS, TypeORM, PostgreSQL, Swagger, global pipes and filter

---
+++ src/main.ts
@@ -0,0 +1,43 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true, 
+    }),
+  );
+
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  app.enableCors({
+    origin: 'http://localhost:3000', //next.js default port
+    credentials: true,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Clinic Management System API')
+    .setDescription('Backend API for managing doctors, patients, and appointments')
+    .setVersion('1.0')
+    .addBearerAuth() 
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  const port = process.env.PORT || 4000;
+  await app.listen(port);
+  console.log(`Clinic API running on: http://localhost:${port}/api`);
+  console.log(`Swagger docs at:       http://localhost:${port}/api/docs`);
+}
+
+bootstrap();
