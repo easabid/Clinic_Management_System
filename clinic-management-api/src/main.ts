99324b177ea9686f70b5dae7c9f02967c21a2ff5
AutomoJapan <bike.racer.sabid@gmail.com>
2026-05-15 17:29:23 +0600

    fix - allow both ports

--- clinic-management-api/src/main.ts
+++ clinic-management-api/src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
   app.useGlobalFilters(new HttpExceptionFilter());
 
   app.enableCors({
-    origin: 'http://localhost:3000', //next.js default port
+    origin: ['http://localhost:3000', 'http://localhost:3001'], 
     credentials: true,
   });
 
