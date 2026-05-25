8ac7ad3cf7b57431177995c046a133582d3a10b3
easabid <bike.racer.sabid@gmail.com>
2026-05-26 00:44:40 +0600

    Remove root duplicate folders

--- src/common/guards/jwt-auth.guard.ts
+++
@@ -1,6 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-
-// wrapper passport's JWT guard
-@Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
--- src/doctors/dto/update-doctor-profile.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateDoctorProfileDto } from './create-doctor-profile.dto';
-
-export class UpdateDoctorProfileDto extends PartialType(
-  CreateDoctorProfileDto,
-) {}
